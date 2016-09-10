7e5c7a9d8061ee83c1656bfd4dd23cc3a58320c0
Tao Wen <taowen@gmail.com>
2016-09-10 15:55:12 +0800

    update

--- functional/main.py
+++ functional/main.py
@@ -3,7 +3,7 @@ import unittest
 
 class Test(unittest.TestCase):
     def test_001(self):
-        //! [aaa]
+//! [aaa]
         for i in xrange(6):
             print(i ** 2)
-        //! [aaa]
+//! [aaa]
